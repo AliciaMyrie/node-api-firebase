da913b76e3448b46eca2af35d8bf55e048e23f4a
Alicia Myrie <aliciamyrie@gmail.com>
2022-07-28 15:48:45 -0400

    deployed firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,20 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+//set up routes
+app.get("/test", (req, res) => {
+  res.send("Wow, its actually worked");
+});
+
+app.get("/another-one", (req, res) => {
+  res.send("this one works, too!");
+});
+
+exports.api = functions.https.onRequest(app); // any response coming in is being sent to express
 
 exports.helloThere = functions.https.onRequest((req, res) => {
   res.send("Hey girl, it's your first Firebase!");
